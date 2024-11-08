c0258752358ce444c2e1f369ce4718e6f6805e7f
skbnsl <bansalsagar13@gmail.com>
2024-11-08 16:02:19 +0530

    category module complete

--- src/main/java/net/javaguides/expense/controller/CategoryController.java
+++ src/main/java/net/javaguides/expense/controller/CategoryController.java
@@ -2,13 +2,13 @@ package net.javaguides.expense.controller;
 
 import lombok.AllArgsConstructor;
 import net.javaguides.expense.dto.CategoryDto;
+import net.javaguides.expense.entity.Category;
 import net.javaguides.expense.service.CategoryService;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @AllArgsConstructor
 @RestController
@@ -21,8 +21,33 @@ public class CategoryController {
     @PostMapping
     public ResponseEntity<CategoryDto> createCategory(@RequestBody CategoryDto categoryDto){
         CategoryDto category = categoryService.createCategory(categoryDto);
+         return new ResponseEntity<>(category, HttpStatus.CREATED);
+    }
 
-        return new ResponseEntity<>(category, HttpStatus.CREATED);
+    @GetMapping("{id}")
+    public ResponseEntity<CategoryDto> getCategoryById(@PathVariable("id") Long categoryId){
+        CategoryDto category = categoryService.getCategoryById(categoryId);
+        return new ResponseEntity<>(category, HttpStatus.OK);
+    }
+
+    @GetMapping
+    public ResponseEntity<List<CategoryDto>> getAllCategories(){
+        List<CategoryDto> categories = categoryService.getAllCategories();
+        return ResponseEntity.ok(categories);
+    }
+
+    @PutMapping("{id}")
+    public ResponseEntity<CategoryDto> updateCategory(@PathVariable("id") Long id,
+                                                      @RequestBody CategoryDto categoryDto){
+
+        CategoryDto updatecategory = categoryService.updateCategory(id, categoryDto);
+        return ResponseEntity.ok(updatecategory);
+    }
+
+    @DeleteMapping("{id}")
+    public ResponseEntity<String> deleteCategory(@PathVariable("id") Long categoryId){
+        categoryService.deleteCategory(categoryId);
+        return ResponseEntity.ok("category deleted successfully!!");
     }
 
 }
--- src/main/java/net/javaguides/expense/service/impl/CategoryServiceImpl.java
+++ src/main/java/net/javaguides/expense/service/impl/CategoryServiceImpl.java
@@ -7,13 +7,17 @@ import net.javaguides.expense.mapper.CategoryMapper;
 import net.javaguides.expense.repository.CategoryRepository;
 import net.javaguides.expense.service.CategoryService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.context.config.ConfigDataResourceNotFoundException;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.stream.Collectors;
 
 @AllArgsConstructor
 @Service
 public class CategoryServiceImpl implements CategoryService {
 
-
+    @Autowired
     private CategoryRepository categoryRepository;
 
     @Override
@@ -25,6 +29,35 @@ public class CategoryServiceImpl implements CategoryService {
         Category savedCategory = categoryRepository.save(category);
 
         return CategoryMapper.mapToCategoryDto(savedCategory);
+    }
+
+    @Override
+    public CategoryDto getCategoryById(Long categoryId) {
+        Category category = categoryRepository.findById(categoryId).orElseThrow(()-> new RuntimeException("category not found with given id: "+categoryId));
+        return CategoryMapper.mapToCategoryDto(category);
+    }
+
+    @Override
+    public List<CategoryDto> getAllCategories() {
+        List<Category> categories = categoryRepository.findAll();
+        return categories.stream().map((category ->
+                    CategoryMapper.mapToCategoryDto(category))).collect(Collectors.toList());
+    }
+
+    @Override
+    public CategoryDto updateCategory(Long categoryId, CategoryDto categoryDto) {
+        Category category = categoryRepository.findById(categoryId).orElseThrow(()->new RuntimeException("category not found with given id: "+categoryId));
+
+        //uodate the categry entity objest and save to db
+        category.setName(categoryDto.name());
+        Category updatedCategory = categoryRepository.save(category);
+        return CategoryMapper.mapToCategoryDto(updatedCategory);
+    }
+
+    @Override
+    public void deleteCategory(Long categoryId) {
+         Category category = categoryRepository.findById(categoryId).orElseThrow(()->new RuntimeException("category not found with given id: "+categoryId));
+         categoryRepository.delete(category);
     }
 
 
